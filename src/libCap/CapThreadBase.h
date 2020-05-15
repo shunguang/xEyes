d70789ae53791261303ec29bed0336f678e9d6c0
swu <shunguang.wu@gmail.com>
2020-05-15 15:44:24 -0400

    add to handle logic in eos_cb()

--- src/libCap/CapThreadBase.h
+++ src/libCap/CapThreadBase.h
@@ -14,7 +14,6 @@ namespace xeyes {
 		CapThreadBase( const CapThreadBase &x) = delete;
 		virtual ~CapThreadBase();
 		void setDetPtr( DetThreadBase *p );
-		virtual void forceQuit();
 
 	protected:
 		//set <isEmitSignal> as false when doing single thread none GUI test
